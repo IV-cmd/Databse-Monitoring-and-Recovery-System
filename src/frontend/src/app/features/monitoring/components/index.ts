e0febdb158a3fc462015a5bbc6880f273124a6ae
IV-cmd <kanaprilla@gmail.com>
2026-05-06 16:16:51 +0200

    Fixed import error

---
+++ src/frontend/src/app/features/monitoring/components/index.ts
@@ -0,0 +1,4 @@
+export { MonitoringComponent } from './monitoring.component';
+export { AlertsPanelComponent } from './alerts-panel/alerts-panel.component';
+export { MetricsChartComponent } from './metrics-chart/metrics-chart.component';
+export { StatusIndicatorComponent } from './status-indicator/status-indicator.component';
