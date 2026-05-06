72a565606c2b85380838af4b16d9f9e6e8c212de
IV-cmd <kanaprilla@gmail.com>
2026-05-06 20:01:46 +0200

    Updated routes for both components

--- src/frontend/src/app/features/dashboard/dashboard.module.ts
+++ src/frontend/src/app/features/dashboard/dashboard.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard.component';
+import { QuickActionsComponent } from './components/quick-actions/quick-actions.component';
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    QuickActionsComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
-      { path: '', component: DashboardComponent }
+      { path: '', component: DashboardComponent },
+      { path: 'quick-actions', component: QuickActionsComponent }
     ])
+  ],
+  exports: [
+    DashboardComponent,
+    QuickActionsComponent
   ]
 })
 export class DashboardModule { }
