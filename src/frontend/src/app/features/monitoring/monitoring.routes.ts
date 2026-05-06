c093920cd1ac4105a1f24e5356b116272d62bb4a
IV-cmd <kanaprilla@gmail.com>
2026-05-06 16:16:25 +0200

    Added monitoring routes

---
+++ src/frontend/src/app/features/monitoring/monitoring.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { MonitoringComponent } from './components';
+
+export const monitoringRoutes: Routes = [
+  {
+    path: '',
+    component: MonitoringComponent
+  }
+];
